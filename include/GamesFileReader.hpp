1ca0081dd81cfda2ae9d0eb8d16d85daac99d4df
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2020-09-15 00:45:20 -0700

    Continue development.

---
+++ include/GamesFileReader.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Games.hpp"
+#include "TextFileReader.hpp"
+
+namespace CatanLeaderboard {
+
+class GamesFileReader : public TextFileReader {
+
+public:
+
+  GamesFileReader(const std::experimental::filesystem::path& path) noexcept : TextFileReader(path) {
+    message("Reading the games file...");
+  }
+
+  Games data() const noexcept {
+    std::vector<Game> data;
+    for (const std::string& line : lines_) {
+      if (!remove_whitespace(line).empty()) {
+        data.emplace_back(line);
+      }
+    }
+    std::sort(data.begin(), data.end(), sort_by_ascending_date());
+    return {data};
+  }
+
+};
+
+} // namespace CatanLeaderboard
